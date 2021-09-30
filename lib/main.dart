8e60871e94d644f0e0a142a092648e128aa4496d
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2021-09-30 09:48:06 +0530

    added loginscreen

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:audio_learn/screens/LoginScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
